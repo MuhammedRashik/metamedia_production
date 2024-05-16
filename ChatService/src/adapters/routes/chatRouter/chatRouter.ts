7ed93a74da2ce9bc9f5b48aa8fb8c43a307c3080
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-16 17:41:32 +0530

    File name changed

--- ChatService/src/adapters/routes/chatRouter/chatRouter.ts
+++ ChatService/src/adapters/routes/chatRouter/chatRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { ChatController, } from '../../Controllers'
+import { ChatController, } from '../../controllers'
 import { upload } from '../../../Utils/multer/multer'
 import authMiddleware from '../../../Utils/Middleware/authMiddleware'
 export default (dependencies:any)=> {
