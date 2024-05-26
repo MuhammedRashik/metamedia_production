57a5ae010a1e4aaf497109072ef75835ff111c2b
Razik <razik.@Raziks-MacBook-Air.local>
2024-05-26 09:56:18 +0530

    meta socket multiple users function settings

--- MetaMedia_Frontend/src/components/MetaComponents/MetaHome.tsx
+++ MetaMedia_Frontend/src/components/MetaComponents/MetaHome.tsx
@@ -14,100 +14,86 @@ const MetaHome=()=>{
     const [users, setUsers] = useState([]);
     const userData = useSelector((state: any) => state.persisted.user.userData);
     const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
-const socket:any=useSocket()
+     const socket:any=useSocket()
+
+ 
+useEffect(() => {
+  if (socket) {
+    const dataPayload = { userId: userData.userId };
+    socket.emit("addNewUserToMeta", dataPayload);
+
+    socket.on("updateUsers", (users:any):any => {
+      toast.success('User list updated');
+      setUsers(users);
+    });
+
+    socket.on("userPositionUpdated", ({ userId, position }:any) => {
+      console.log('getiing',position);
+      
+      setUsers((prevUsers:any) =>
+        prevUsers.map((user:any) =>
+          user.userId === userId ? { ...user, position } : user
+        )
+      );
+    });
+
+    return () => {
+      socket.off("updateUsers");
+      socket.off("userPositionUpdated");
+    };
+  }
+}, [socket]);
 
 useEffect(() => {
-    if (socket) {
-      const dataPayload = { userId: userData.userId };
-      socket.emit("addNewUserToMeta", dataPayload);
-
-      socket.on("updateUsers", (users:any):any => {
-        toast.success('User list updated');
-        setUsers(users);
-      });
- 
-      socket.on("userPositionUpdated", ({ userId, position }:any) => {
-        console.log('getiing',position);
-        
-        setUsers((prevUsers:any) =>
-          prevUsers.map((user:any) =>
-            user.userId === userId ? { ...user, position } : user
-          )
-        );
-      });
-
-      return () => {
-        socket.off("updateUsers");
-        socket.off("userPositionUpdated");
-      };
+  const handleKeyDown = (event:any) => {
+    const moveDistance = 0.1;
+    let newPosition = { ...position };
+    switch (event.key) {
+      case 'ArrowUp':
+        newPosition.z -= moveDistance;
+        break;
+      case 'ArrowDown':
+        newPosition.z += moveDistance;
+        break;
+      case 'ArrowLeft':
+        newPosition.x -= moveDistance;
+        break;
+      case 'ArrowRight':
+        newPosition.x += moveDistance;
+        break;
+      default:
+        return;
     }
-  }, [socket]);
+    setPosition(newPosition);
+   
+    
+   if(socket){
+    console.log(newPosition);
+    socket.emit("setUserPosition", { userId: userId, position: newPosition });
+   }
+  };
 
-
-  useEffect(() => {
-    const handleKeyDown = (event:any) => {
-      const moveDistance = 0.1;
-      let newPosition = { ...position };
-      switch (event.key) {
-        case 'ArrowUp':
-          newPosition.z -= moveDistance;
-          break;
-        case 'ArrowDown':
-          newPosition.z += moveDistance;
-          break;
-        case 'ArrowLeft':
-          newPosition.x -= moveDistance;
-          break;
-        case 'ArrowRight':
-          newPosition.x += moveDistance;
-          break;
-        default:
-          return;
-      }
-      setPosition(newPosition);
-     
-      
-     if(socket){
-      console.log(newPosition);
-      socket.emit("setUserPosition", { userId: userId, position: newPosition });
-     }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [position, socket]);
+  window.addEventListener('keydown', handleKeyDown);
+  return () => {
+    window.removeEventListener('keydown', handleKeyDown);
+  };
+}, [position, socket]);
 
 
     return (
         <>
         <div className="w-screen h-screen fixed">
-
-       <Canvas shadows  >
-     
-        <ambientLight intensity={2}/>
-       <directionalLight  />
-      
-       
-     
-  
-  <Physics gravity={[0, -6.003, 0]} allowSleep={false} broadphase="SAP">
-  {/* <WhiteTshirtGirlModel /> */}
-  {users && users.map((user:any) => {
-    return (
-        <>
-        <Box position={user.position} />
-        </>
-    )
-  })}
-
-   
-     <HomeTownModel />
-   
-    </Physics> 
-       </Canvas>
-        </div>
+      <Canvas shadows>
+        <ambientLight intensity={2} />
+        <directionalLight />
+        <Physics gravity={[0, -6.003, 0]} allowSleep={false} broadphase="SAP">
+          {users.map((user) => (
+            <Box position={user.position} />
+            ))}
+          <HomeTownModel />
+        </Physics>
+      </Canvas>
+    </div>
 
        
         </>
