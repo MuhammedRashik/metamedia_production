e30856e4549741b7ed875a17c4338165dd6b3745
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-27 20:16:03 +0530

    Added accesstoken data

--- StoryService/src/Util/Jwt/jwt.ts
+++ StoryService/src/Util/Jwt/jwt.ts
@@ -2,8 +2,10 @@ export const decodeDataFromHeaders = (headers: any) => {
   const { decodedTokenData } = headers;
   console.log(decodedTokenData,"decodedTokenData");
   
+  console.log(decodedTokenData.user?.response?.basicInformation,"decodedTokenData");
+  
     if (decodedTokenData.user) {
-      const userId = decodedTokenData?.user?._id ||  decodedTokenData?.user?.user?._id;
+      const userId = decodedTokenData?.user?._id ||  decodedTokenData?.user?.user?._id || decodedTokenData.user.response?.basicInformation.userId
             return userId
   }else{
     return {status:false , message:"user Not Exist"}
