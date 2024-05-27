849519c5443bf5593ca4377f3d77dc432dff82ac
Razik <razik.@Raziks-MacBook-Air.local>
2024-05-27 11:33:24 +0530

    multiple carecter rendering debuging

--- MetaMedia_Frontend/src/components/MetaComponents/MetaHome.tsx
+++ MetaMedia_Frontend/src/components/MetaComponents/MetaHome.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { Canvas } from 'react-three-fiber'
 import { Physics } from '@react-three/cannon'
 import { useSelector } from 'react-redux'
@@ -7,44 +7,32 @@ import { useSocket } from '../../utils/costumHook/useSoket'
 import { WhiteTshirtGirlModel } from './Models/WhiteTshirtGirl'
 import { HomeTownModel } from './Models/Home'
 import * as THREE from 'three'
-import {BalckManModel} from '../MetaComponents/Models/BlackMan'
-import { OrbitControls } from '@react-three/drei'
-import { group } from 'console'
+import { BalckManModel } from '../MetaComponents/Models/BlackMan'
 
 const MetaHome = () => {
- 
   const userData = useSelector((state: any) => state.persisted.user.userData)
-// const userData={
-//     userId:"hi"
-// }
   const socket: any = useSocket()
   const [position, setPosition] = useState(new THREE.Vector3(0, 0, 0))
-  const [camaraPosition,setCamaraPosition]=useState(new THREE.Vector3(0,0,0))
-  const [users, setUsers]:any = useState([])
+  const [cameraPosition, setCameraPosition] = useState(new THREE.Vector3(0, 0, 0))
+  const [users, setUsers] = useState<any[]>([])
+
   useEffect(() => {
     if (socket) {
-      const dataPayload = { 
-        
-        userId: userData.userId ,
-        position:position
-    
-    }
-      socket.emit("addNewUserToMeta", (dataPayload))
+      const dataPayload = { userId: userData.userId, position }
+      socket.emit("addNewUserToMeta", dataPayload)
 
-      socket.on("updateUsers", (users: any): any => {
+      socket.on("updateUsers", (users: any) => {
         toast.success('User list updated')
         setUsers(users)
       })
 
       socket.on("userPositionUpdated", ({ userId, position }: any) => {
-
         if (userId === userData.userId) {
           setPosition(new THREE.Vector3(position.x, position.y, position.z))
         }
-        // const newPosition=new THREE.Vector3(user.position.x,user.position.y,user.position.z)
         setUsers((prevUsers: any) =>
           prevUsers.map((user: any) =>
-            user.userId === userId ? { ...user, position } : user
+            user.userId === userId ? { ...user, position: new THREE.Vector3(position.x, position.y, position.z) } : user
           )
         )
       })
@@ -56,18 +44,19 @@ const MetaHome = () => {
     }
   }, [socket, userData.userId])
 
-  useEffect(()=>{
-console.log(users,'USERSS');
-  },[users])
+  useEffect(() => {
+    console.log(users, 'USERSS')
+  }, [users])
+
   return (
     <div className="w-screen h-screen fixed">
       <Canvas shadows>
         <ambientLight intensity={2} />
         <directionalLight />
         <Physics gravity={[0, -6.003, 0]} allowSleep={false} broadphase="SAP">
-          {users &&  users.map((user) => (
+          {users.map((user) => (
             <group key={user.userId}>
-            <BalckManModel  position={new THREE.Vector3(user.position.x,user.position.y,user.position.z)}  />
+              <BalckManModel position={user.position} />
             </group>
           ))}
           <HomeTownModel />
