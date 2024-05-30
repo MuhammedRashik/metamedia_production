a6d102dae8f5d032914c1d0b74a2ceb98418d987
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-30 09:21:06 +0530

    Cookie data done

--- MetaMedia_Frontend/src/Routes/ProtectedAuthRoute.tsx
+++ MetaMedia_Frontend/src/Routes/ProtectedAuthRoute.tsx
@@ -1,8 +1,19 @@
 import { Navigate } from "react-router-dom";
+import Cookies from 'js-cookie';
+import { useEffect, useState } from "react";
 
 const ProtectedAuthRoute = ({ children }: { children: any }) => {
-  const token = cook.getItem('accesstoken')
-    if (token) {
+  const [cookieValue, setCookieValue] = useState('');
+
+  useEffect(() => {
+    // Get the cookie value by its name
+    const token = Cookies.get('accesstoken');
+    console.log(token,"cook");
+    
+    setCookieValue(token);
+  }, []);
+  // const token = .getItem('accesstoken')
+    if (cookieValue) {
       return children
     }else{
       return <Navigate to="/login" replace />
