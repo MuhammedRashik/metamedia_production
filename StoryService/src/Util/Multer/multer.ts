2776e94bc7a00b0333c495fa5c9958b0d5ec3c16
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-30 12:24:05 +0530

    multer error

--- StoryService/src/Util/Multer/multer.ts
+++ StoryService/src/Util/Multer/multer.ts
@@ -1,15 +1,24 @@
-
-import multer from "multer"
+import multer from "multer";
+import fs from "fs";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    return cb(null, "public/story/")
-  },  
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + file.originalname)
+    const dir = path.join(__dirname, "public/story");
+
+    // Check if the directory exists
+    if (!fs.existsSync(dir)) {
+      // Create the directory if it does not exist
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    cb(null, dir);
   },
-})
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
 export const upload = multer({
-    storage:storage
-})
+  storage: storage,
+});
