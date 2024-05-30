4461714382595ec9c6e25b6fe83890938d777b2e
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-05-30 10:17:08 +0530

    prottected route set

--- MetaMedia_Frontend/src/Routes/ProtectedAuthRoute.tsx
+++ MetaMedia_Frontend/src/Routes/ProtectedAuthRoute.tsx
@@ -1,27 +1,22 @@
-import { Navigate } from "react-router-dom";
-import Cookies from 'js-cookie';
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedAuthRoute = ({ children }: { children: any }) => {
   const [cookieValue, setCookieValue] = useState('');
 
-    // Get the cookie value by its name
-    const token = localStorage.getItem('accesstoken')
-    console.log(token,"tokk");
-    
+  useEffect(() => {
+    const token = localStorage.getItem('accesstoken');
+    console.log(token, "tokk");
     setCookieValue(token);
-  // const token = .getItem('accesstoken')
-    if (cookieValue) {
-      console.log(cookieValue,"cookieValue");
-      
-      return children
-    }else{
-      console.log("ProtectedAuthRoute to login");
-      
-      return <Navigate to="/login" replace />
-     
-    }
+  }, []);
 
-  };
-  
-  export default ProtectedAuthRoute;
+  if (cookieValue) {
+    console.log(cookieValue, "cookieValue");
+    return children;
+  } else {
+    console.log("ProtectedAuthRoute to login");
+    return <Navigate to="/login" replace />;
+  }
+};
+
+export default ProtectedAuthRoute;
