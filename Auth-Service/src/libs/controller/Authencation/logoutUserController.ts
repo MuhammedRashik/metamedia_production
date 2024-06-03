8fff9216735ce58b50ce6ab008daac3d38d9e2b5
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-06-03 14:21:01 +0530

    Local storage access token removed

--- Auth-Service/src/libs/controller/Authencation/logoutUserController.ts
+++ Auth-Service/src/libs/controller/Authencation/logoutUserController.ts
@@ -5,10 +5,10 @@ import { Cookie } from "express-session";
 export default (dependencies: any) => {
   const logoutController = (req: Request, res: Response) => {
     console.log("logoutController");
-    
     try {
     clearAccessTokenFromCookie("accessToken",res)
-    res.clearCookie("accessToken");
+    res.clearCookie('accessToken');
+    localStorage.removeItem('accesstoken')
       res.json({ status: true, message: "Logout success" });
     } catch (err) {
         console.log(err,"er");
