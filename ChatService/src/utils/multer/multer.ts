8e4f9ee544b30f8d5f2fa50ea95169cd3a6060a1
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-06-03 17:17:10 +0530

    Chat image err solved

--- ChatService/src/utils/multer/multer.ts
+++ ChatService/src/utils/multer/multer.ts
@@ -5,8 +5,8 @@ import path from "path";
 const storage: multer.StorageEngine = multer.diskStorage({
   destination: function (req: Request, file: Express.Multer.File, callback: (error: Error | null, destination: string) => void) {
       console.log('hiiiiiiiiiiiii');
-      console.log(__dirname,"__dirname");      
-      callback(null, path.join(__dirname, '../../../public/chat'));
+      
+      callback(null, path.join('public/chat'));
   },
   filename: function (req: Request, file: Express.Multer.File, callback: (error: Error | null, filename: string) => void) {
       callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
