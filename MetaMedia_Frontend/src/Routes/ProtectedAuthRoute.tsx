cec373669b7679ed404bbc50c6bf9688ab8e6119
AbleKSaju <126228406+AbleKSaju@users.noreply.github.com>
2024-06-03 15:25:11 +0530

    Optional chaining added

--- MetaMedia_Frontend/src/Routes/ProtectedAuthRoute.tsx
+++ MetaMedia_Frontend/src/Routes/ProtectedAuthRoute.tsx
@@ -7,12 +7,12 @@ const ProtectedAuthRoute = ({ children }: { children: any }) => {
   const userData=useSelector((state:any)=>state.persisted.user.userData)
 console.log(userData,"userData");
 
-  if (!token && !userData) {
+  if (!token && !userData.userId) {
     console.log("ProtectedAuthRoute to login");
     return <Navigate to="/login" replace />;
   }
-
   if (token) {
+    console.log("I AM WITH TOKEN");
     console.log(token, "localStorage token");
     return children;
   } else {
